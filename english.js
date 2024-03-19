f4704dc2437954ee42cb1ec994dcfa5d0c82481a
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-03-19 17:46:35 +0000

    fix correction

--- english.js
+++ english.js
@@ -1,15 +1,8 @@
-// funcao para corrigir a questao, dizendo qual a resposta correta 
 
-// etapas:
-// 1- ao apertar o botao ira ativar a funcao de corrigir a questao 
 
-// 2- a funcao vai procurar no codigo a alternativa com a id "correct"
-
-// 3- apos pegar a alternativa correta vai mudar o background para outra cor  
-
-function correction() {
-    const getElement = document.getElementById("correct");
+function correction(id) {
+    const getElement = document.getElementById(id);
     getElement.style.background = "green";
-    
- 
+     
 }
+ 
