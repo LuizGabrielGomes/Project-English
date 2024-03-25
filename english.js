a4f8a044ca0de4b0f15d08b3d96e56c8c8a56dd1
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-03-25 17:00:08 +0000

    refartoring questions

--- english.js
+++ english.js
@@ -7,24 +7,71 @@ function correction(id) {
 }
 
 
-
-
 const questions = [
-    "What is the meaning of find out?",
-    "What is the mean of back up?", 
-    "What is the mean of Carry on?",  
-    "What is the mean of Get along/ Get on/ Get on with?"
+    { 
+        _id: 1,
+        title: "What is the meaning of find out?",
+        alternatives: [  
+            "Discover a fact or piece of information",
+            "Means to eventually decide something",  
+            "To have a good relationship with someone",  
+        ]
+    },
+    { 
+        _id: 2, 
+        title: "What is the mean of back up?",
+        alternatives: [  
+            "Return to the place",
+            "Do a review",  
+            "Support or defend someone",  
+        ]
+    },
+    { 
+        _id: 3,
+        title: "What is the mean of Carry on?",
+        alternatives: [  
+            "To Cancel something",
+            "Continue",  
+            "discover or learn",  
+        ]
+    },
+    { 
+        _id: 4,
+        title: "What is the mean of Get along/ Get on/ Get on with?",
+        alternatives: [  
+            "stay for a long time",
+            "take a long time",  
+            "To have a good relationship",  
+        ]
+    },
+
 ]
 
 function question(){
-    let element = questions.map((item,index) => {
+    let element = questions.map((item, index) => {
         console.log(item);
+
         const title = document.createElement("div")
         title.classList.add("title")
         const paragraph = document.createElement("p")
         paragraph.id = "title"
+        paragraph.textContent = item.title
+        
+        const containerAlternatives = document.createElement("div")
+
+        const alternatives = item.alternatives.map((alternative, index) => {
+          console.log(alternative)  
+
+          const alternatives = document.createElement("p")
+          alternatives.textContent = alternative 
+          return alternatives 
+        })
+        console.log(alternatives)
+        containerAlternatives.appendChild(alternatives)
+
+        console.log(containerAlternatives);
         title.appendChild(paragraph)
-        paragraph.textContent = questions[index]
+        
         return title
         
     })
